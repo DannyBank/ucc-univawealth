741daf097c733183ae7cdc73c6289d1ec79e9e7e
Dbank <dbucknor-ankrah@acsint.io>
2026-07-05 00:35:15 +0000

    Chore (App): Fixed error in build by removing javafx-web ref

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,10 @@
 module com.dbank.uccunivawealth {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
-    requires eu.hansolo.tilesfx;
 
     opens com.dbank.uccunivawealth to javafx.fxml;
     exports com.dbank.uccunivawealth;
