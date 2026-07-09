b2a12f00951c66ea2e3d96f7e77736f17b8e37e3
Dbank <dbucknor-ankrah@acsint.io>
2026-07-09 17:35:35 +0000

    Chore (App): Re-setup database seeding

--- database/scripts/category-seed.sql
+++ database/scripts/category-seed.sql
@@ -1,10 +1,14 @@
 -- seed data for category
 
 insert into Categories(CategoryId, CategoryName)
-values (1, "Deposit"),
-       (2,"Withdrawal"),
-       (3,	"Investment"),
-       (4,	"Interest"),
-       (5,	"Transfer"),
-       (6,	"Dividend"),
-       (7,	"Penalty)")
+values (1,	'Deposit'),
+(2,	'Withdrawal'),
+(3,	'Investment Purchase'),
+(4,	'Interest'),
+(5,	'Transfer'),
+(6,	'Dividend'),
+(7,	'Penalty'),
+(8,	'Goal'),
+(9,	'Savings'),
+(10,	'Login'),
+(11,	'Register')
